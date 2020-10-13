ea191f3c85c2a9699818a64c5661185db98299a7
Denddy <denddyrizky@gmail.com>
2020-10-13 14:29:18 +0200

    Finished the DAO and database

--- app/src/main/java/com/example/madlevel4task2/GameRoomDatabase.kt
+++ app/src/main/java/com/example/madlevel4task2/GameRoomDatabase.kt
@@ -1,4 +1,30 @@
 package com.example.madlevel4task2
 
-class GameRoomDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Game::class], version = 1, exportSchema = false)
+abstract class GameRoomDatabase : RoomDatabase() {
+
+    abstract fun gameDao() : GameDao
+
+    companion object{
+        private const val DATABASE_NAME = "GAME_DATABASE"
+
+        @Volatile
+        private var gameRoomDatabaseInstace: GameRoomDatabase? = null
+
+        fun getDatabase(context: Context) : GameRoomDatabase?{
+            if (gameRoomDatabaseInstace == null){
+                synchronized(GameRoomDatabase::class.java){
+                    if (gameRoomDatabaseInstace == null){
+                        gameRoomDatabaseInstace = Room.databaseBuilder(context.applicationContext, GameRoomDatabase::class.java, DATABASE_NAME).build()
+                    }
+                }
+            }
+            return gameRoomDatabaseInstace
+        }
+    }
 }

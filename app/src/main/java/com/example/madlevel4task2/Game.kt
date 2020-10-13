6b870297eb81e29582ba51de2e5667539347e080
Denddy <denddyrizky@gmail.com>
2020-10-13 13:55:55 +0200

    Made the game data class

--- app/src/main/java/com/example/madlevel4task2/Game.kt
+++ app/src/main/java/com/example/madlevel4task2/Game.kt
@@ -1,4 +1,22 @@
 package com.example.madlevel4task2
 
-class Game {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "gameTable")
+data class Game(
+
+    @ColumnInfo(name = "computerMove")
+    var computerMove: Int,
+
+    @ColumnInfo(name = "playerMove")
+    var playerMove: Int,
+
+    @ColumnInfo(name = "gameResult")
+    var gameResult: Int,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long? = null
+)
